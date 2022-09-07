8259377861f3261edd2f4d609601d27ce2dc4992
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-09-07 19:00:44 +0300

    Change names of variables

--- main.py
+++ main.py
@@ -15,27 +15,27 @@ def main():
         return
 
     try:
-        pixel_image = Image.open("pixel.jpg")
+        image_for_pixel = Image.open("pixel.jpg")
     except FileNotFoundError:
         write_to_console(
             "Файл 'pixel.jpg' не обнаружен. Проверьте его наличие в папке с программой и запустите её снова.")
         return
 
-    pixels_original = original.load()
-    xOrig, yOrig = original.size
-    xPixel, yPixel = pixel_image.size
+    original_pixels = original.load()
+    original_width, original_height = original.size
+    pixel_image_width, pixel_image_height = image_for_pixel.size
 
-    number_of_pixels = xOrig * xPixel * yOrig * yPixel
+    number_of_pixels = original_width * pixel_image_width * original_height * pixel_image_height
     if number_of_pixels >= 10 ** 13:
-        if not confirm_working(xOrig * xPixel, yOrig * yPixel, "Это, скорее всего, сожжёт ваш компьютер."):
+        if not confirm_working(original_width * pixel_image_width, original_height * pixel_image_height, "Это, скорее всего, сожжёт ваш компьютер."):
             write_to_console("Операция прервана.")
             return
     elif number_of_pixels >= 10 ** 10:
-        if not confirm_working(xOrig * xPixel, yOrig * yPixel, "Это займёт гигантское количество времени."):
+        if not confirm_working(original_width * pixel_image_width, original_height * pixel_image_height, "Это займёт гигантское количество времени."):
             write_to_console("Операция прервана.")
             return
     elif number_of_pixels >= 10 ** 7:
-        if not confirm_working(xOrig * xPixel, yOrig * yPixel, "Это займёт большое количество времени."):
+        if not confirm_working(original_width * pixel_image_width, original_height * pixel_image_height, "Это займёт большое количество времени."):
             write_to_console("Операция прервана.")
             return
 
@@ -56,27 +56,26 @@ def main():
     write_to_console("Создание изображения нужных размеров...")
     result = Image.new('RGB', (1, 1), color='red')
     try:
-        result = Image.new('RGB', (xOrig * xPixel, yOrig * yPixel), color='red')
+        result = Image.new('RGB', (original_width * pixel_image_width, original_height * pixel_image_height), color='red')
     except MemoryError:
         write_to_console("Создаваемое изображение является СЛИШКОМ большим. Уменьшите разрешения "
                          "изображений 'pixel.jpg' и 'source.jpg' и попробуйте сначала.")
     pixels = result.load()
-    loading_final = (xOrig - 1) / 100
+    loading_final = (original_width - 1) / 100
 
-    for x in range(xOrig):
+    for x in range(original_width):
         loading = round(x / loading_final, 3)
         write_to_console(f"Обработка - {loading}%")
-        for y in range(yOrig):
-
-            middleR = visible * pixels_original[x, y][0]
-            middleG = visible * pixels_original[x, y][1]
-            middleB = visible * pixels_original[x, y][2]
-            for pixelX in range(xOrig):
-                for pixelY in range(yOrig):
-                    orig = pixels_original[pixelX, pixelY]
-                    pixels[pixelX + xOrig * x, pixelY + yOrig * y] = (
-                        int(orig[0] * negative_visible + middleR), int(orig[1] * negative_visible + middleG),
-                        int(orig[2] * negative_visible + middleB))
+        for y in range(original_height):
+            r_original = visible * original_pixels[x, y][0]
+            g_original = visible * original_pixels[x, y][1]
+            b_original = visible * original_pixels[x, y][2]
+            for pixelX in range(original_width):
+                for pixelY in range(original_height):
+                    pixel = original_pixels[pixelX, pixelY]
+                    pixels[pixelX + original_width * x, pixelY + original_height * y] = (
+                        int(pixel[0] * negative_visible + r_original), int(pixel[1] * negative_visible + g_original),
+                        int(pixel[2] * negative_visible + b_original))
     write_to_console("Сохранение изображения...")
     result.save(f"{name_result}.jpg")
     write_to_console("Изображение сохранено.")
